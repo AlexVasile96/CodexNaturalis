bbc9611a2f79d6b6395eec42c8c840f26dbca22f
CallanDragon <10774521@polimi.it>
2024-04-08 23:05:33 +0200

    endgame attributes

--- src/main/java/model/EndGame.java
+++ src/main/java/model/EndGame.java
@@ -6,9 +6,11 @@ public class EndGame {
     private Board board;
     private ResourceDeck resourceDeck;
     private GoldDeck goldDeck;
+    private ObjectiveCard firstCommonObjective;
+    private ObjectiveCard secondCommonObjective;
     //le due carte obiettivo comuni a tutte
     //carta segreta di ogni player
 
- public void finishGame(){}
+    public void finishGame(){}
     public void countObjectiveCards(){}
 }
