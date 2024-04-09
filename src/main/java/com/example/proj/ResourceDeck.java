303ae1e0eefb0e18b84bd6b6d24a9c31acab2684
alex <vladalexandru.vasile@polimi.it>
2024-04-09 11:39:52 +0200

    Test added

--- src/main/java/com/example/proj/ResourceDeck.java
+++ src/main/java/com/example/proj/ResourceDeck.java
@@ -1,4 +1,5 @@
 package com.example.proj;
+
 import java.util.Collections;
 import java.util.List;
 
@@ -17,18 +18,19 @@ public class ResourceDeck implements Deck{
     }
     @Override
     public Card drawCard(Player player) {
-        if (resourceCards.isEmpty()) {
-            return null; // Mazzo vuoto
-        }
-        if(player.getPlayerCards().size()<3) {
-            Card drownCard = resourceCards.remove(0);
-            player.getPlayerCards().add(drownCard);
-            return drownCard;
-        }
-        else{
-            throw new RuntimeException();
-        }
+
+            if (resourceCards.isEmpty()) {
+                return null; // Mazzo vuoto
+            }
+            if (player.getPlayerCards().size() < 3) {
+                Card drownCard = resourceCards.remove(0);
+                player.getPlayerCards().add(drownCard);
+                return drownCard;
+            } else {
+                throw new RuntimeException();
+            }
     }
+
     public void addCard(Card card) {
         resourceCards.add(card);
     }
