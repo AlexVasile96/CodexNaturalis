e217f6b2969d56b05f5eb026368ec7238107b144
CallanDragon <10774521@polimi.it>
2024-04-19 16:57:29 +0200

    Login strings going well - w gang seconda commit del giorno road to 5

--- src/main/java/codex/Codex.java
+++ src/main/java/codex/Codex.java
@@ -12,7 +12,7 @@ import java.util.Scanner;
 
 public class Codex {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         System.out.println("Hi! Welcome to Codex!!!\nWhat do you want to launch?");
         System.out.println(
                 """
