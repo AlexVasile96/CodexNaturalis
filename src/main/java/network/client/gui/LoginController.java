a5ee328b5225bfcc94e211ffba2f013cb0a0c6fa
Thomas Paletti <10831275@polimi.it>
2024-05-25 10:36:16 +0200

    fixed placing cards on corners

--- src/main/java/network/client/gui/LoginController.java
+++ src/main/java/network/client/gui/LoginController.java
@@ -149,4 +149,11 @@ public class LoginController {
             }
         }).start();
     }
+    private void showAlert(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
 }
