baeef1ace7726bb138c995487c652a1f96042419
Thomas Paletti <10831275@polimi.it>
2024-05-26 10:24:46 +0200

    EndGameScene updated

--- src/main/java/network/client/gui/scene/EndGameScene.java
+++ src/main/java/network/client/gui/scene/EndGameScene.java
@@ -2,14 +2,18 @@ package network.client.gui.scene;
 
 import javafx.animation.PauseTransition;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import view.ClientView;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
 
@@ -29,15 +33,24 @@ public class EndGameScene {
         this.clientView = clientView;
     }
 
-    public void endGame(){
+    public void endGame() throws IOException {
         StackPane root = new StackPane();
-        VBox vbox = new VBox();
         Text winner = new Text("The winner is: " );
-        vbox.getChildren().add(winner);
 
-        root.getChildren().add(vbox);
+        Image background = new Image("/SfondoWinner.jpg");
+        BackgroundSize backgroundSize = new BackgroundSize(100, 100, true, true, true, false);
+        BackgroundImage backgroundImage = new BackgroundImage(background, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+        root.setBackground(new Background(backgroundImage));
 
-        primaryStage.setScene(new Scene(root, 800, 600));
+        winner.setFill(Color.WHITE);
+        // Imposta la dimensione del carattere
+        winner.setFont(new Font(24));
+        // Aggiungi un'ombra al testo per migliorare la visibilità
+        winner.setStyle("-fx-effect: dropshadow(one-pass-box, black, 8, 0, 0, 0);");
+
+        root.getChildren().add(winner);
+
+        primaryStage.setScene(new Scene(root, 800, 533));
         primaryStage.setTitle("Winner winner chicken dinner");
         primaryStage.show();
 
@@ -45,15 +58,24 @@ public class EndGameScene {
         Media drumSoundMedia = new Media(drumRollSound);
         MediaPlayer drumRollPlayer = new MediaPlayer(drumSoundMedia);
 
-        drumRollPlayer.play();
-        System.out.println("La durata del file mp3: "+ drumSoundMedia.getDuration());
+        String yeah = getClass().getResource("/Sounds/CSI.mp3").toString();
+        Media yeahMedia = new Media(yeah);
+        MediaPlayer yeahPlayer = new MediaPlayer(yeahMedia);
 
-        PauseTransition delay = new PauseTransition(drumSoundMedia.getDuration());
+
+        PauseTransition delay = new PauseTransition(Duration.seconds(4.25));
+
+        drumRollPlayer.play();
+        System.out.println(in.readLine());
+
+        drumRollPlayer.setOnEndOfMedia(()->{
+            drumRollPlayer.stop();
+            yeahPlayer.play();
+        });
 
         delay.setOnFinished(event -> {
-            winner.setText("The winner is: Paolo");
+            winner.setText("The winner is: Eli");
         });
         delay.play();
-
     }
 }
